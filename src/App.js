a9b4fb654c97773480fc4b04d69dfb6247926dfa
HuguesBriqueler <hugues.briqueler@gmail.com>
2022-03-24 19:42:40 +0100

    Added context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Content from "./component/Content/Content";
 import ToggleLanguage from "./component/ToggleLanguage/ToggleLanguage";
+import ContextProvider from "./context/LanguageContext";
+
 function App() {
   return (
-    <>
+    <ContextProvider>
       <ToggleLanguage />
       <Content />
-    </>
+    </ContextProvider>
   );
 }
 
--- src/component/Content/Content.js
+++ src/component/Content/Content.js
@@ -1,11 +1,14 @@
+import { useContext } from "react";
+import { Context } from "../../context/LanguageContext";
 import styles from "./Content.module.css";
 import data from "../../assets/data";
 
 function Content() {
+  const { language } = useContext(Context);
   return (
     <div className={styles.content}>
-      <h1 className={styles.title}>{data["FR"].title}</h1>
-      <p className={styles.contentTxt}>{data["FR"].txt}</p>
+      <h1 className={styles.title}>{data[language].title}</h1>
+      <p className={styles.contentTxt}>{data[language].txt}</p>
     </div>
   );
 }
